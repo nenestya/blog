d07003f06bcb7d1ef650f7dca8f78b40523154c0
Luky Rahman <lukyrahman68@gmail.com>
2018-11-15 16:08:52 +0700

    fix home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('front.viewpost');
+        $posts = Post::all();
+        return view('front.viewpost', compact('posts'));
     }
 }
